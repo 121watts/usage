abdfe250c106f4f0fda4ab5d207167b3489e92e6
Andrew Watkins <andrew.watkinz@gmail.com>
2020-04-03 12:16:49 -0700

    chore: initial commit

---
+++ lambdas/flux.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch').default
+
+const query = '\n    from(bucket: "system_usage")\n    |> range(start: -24h})\n    |> filter(fn: (r) => r._measurement == "storage_usage_org_cardinality"\n      and r._field == "gauge"\n      and r.org_id != "")\n    |> keep(columns: ["org_id", "_value"])\n    |> group(columns: ["org_id"], mode:"by")\n    |> last()\n'
+
+exports.handler = async (event, context) => {
+  try {
+    const subject = event.queryStringParameters.name || 'World'
+    const res = fetch(
+        "https://us-west-2-1.aws.cloud2.influxdata.com/api/v2/query",
+        {
+            method:"POST",
+            body:JSON.stringify({query})
+        })
+
+    const data = await resp.json()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ data })
+    }
+
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() }
+  }
+}

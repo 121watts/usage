e0a6672d09d0abf6e2d8d2fd83f8ccaa1d0439fd
Andrew Watkins <andrew.watkinz@gmail.com>
2020-04-03 12:56:31 -0700

    chore: log env

--- src/lambdas/flux.js
+++ src/lambdas/flux.js
@@ -2,6 +2,8 @@ const fetch = require('node-fetch').default
 
 const query = '\n    from(bucket: "system_usage")\n    |> range(start: -24h})\n    |> filter(fn: (r) => r._measurement == "storage_usage_org_cardinality"\n      and r._field == "gauge"\n      and r.org_id != "")\n    |> keep(columns: ["org_id", "_value"])\n    |> group(columns: ["org_id"], mode:"by")\n    |> last()\n'
 
+console.log('process.env', process.env)
+
 exports.handler = async (event, context) => {
   try {
     const res = await fetch(
